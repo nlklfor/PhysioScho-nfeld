f70582b1e8c63a90cc877227801165570d97ae18
Nikita <nik.slepcov1771@gmail.com>
2024-01-24 14:53:26 +0100

    header and banner

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+function Banner() {
+    return(
+        <section className={'banner-section'}>
+            <h2 className={'banner-title'}>
+                Physiotherapy practice in Munich Harlaching <br/>We‘ll get you fit for everyday life
+            </h2>
+        </section>
+    )
+ }
+
+ export default Banner;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,48 @@
+import './style.css'
+import logo from '../../assets/Logo.png'
+import phone from '../../assets/phone.svg'
+
+function Header() {
+    return (
+        <>
+            <header className={'header'}>
+                <section className={'header-section'}>
+                    <img src={logo} alt={'logo'}/>
+                    <ul className={'header-nav'}>
+                        <li className={'header-item'}>
+                            About us
+                        </li>
+                        <li className={'header-item'}>
+                            How you can find us
+                        </li>
+                        <li className={'header-item'}>
+                            Service
+                        </li>
+                        <li className={'header-item'}>
+                            Practice
+                        </li>
+                        <li className={'header-item'}>
+                            Contacts
+                        </li>
+                        <li className={'header-item'}>
+                            Career
+                        </li>
+                        <li className={'header-item'}>
+                            Reviews
+                        </li>
+                        <li className={'header-item'}>
+                            <button className={'header-button'}>
+                                <img src={phone} alt={'phone'} className={'phone-img'}/>
+                                <a className={'phone-link'} href="tel:08964913555">089 - 649 135 55</a>
+                            </button>
+                        </li>
+                    </ul>
+
+                </section>
+            </header>
+        </>
+
+    )
+}
+
+export default Header;
